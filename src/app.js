c4324a27ea8afc197764b382fed1e876fe256f0f
4fr0c0d3 <ngigemoffat@gmail.com>
2019-08-27 15:24:09 +0300

    feat(middleware): add middleware
    - add cors
    - add morgan for logging
    - add mongiise for db connections
    - add body-parser to parse requests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+import cors from 'cors';
+import morgan from 'morgan';
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import locationsRoute from './routes/locations';
 
-const app = express()
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.connect('mongodb://localhost/SMS_DB', { useNewUrlParser: true });
 
-app.listen(port, () => console.log(`Population app listening on port ${port}!`))
+app.use(cors());
+app.use(morgan('dev'));
+app.options('*', cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/v1/locations', locationsRoute);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Population app listening on port ${port}!`));
 
 export default app;
